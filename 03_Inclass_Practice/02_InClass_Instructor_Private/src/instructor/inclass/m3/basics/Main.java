bae68673c1fc2eac197c1c4baa3ad6538b51f86d
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 10:15:46 -0700

    Update M3 basics runner for Canvas-aligned constructors

--- 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m3/basics/Main.java
+++ 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m3/basics/Main.java
@@ -3,11 +3,10 @@ package instructor.inclass.m3.basics;
 /** Optional combined runner for the Canvas-aligned Car and Book examples. */
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("Toyota", "Camry");
-        car.start();
+        Car car = new Car("Toyota", "Camry", 2024, 32000.00);
         car.drive();
 
-        Book book = new Book("Core Java", "Cay Horstmann");
-        book.displayInfo();
+        Book book = new Book("Core Java", "Cay Horstmann", 928, 59.99);
+        book.read();
     }
 }
