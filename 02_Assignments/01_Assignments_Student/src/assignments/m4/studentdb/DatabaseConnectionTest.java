acc95a2909c7652166a22d1b898d47d46da9a24f
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 08:58:02 -0700

    Move student M4 database test under assignments

---
+++ 02_Assignments/01_Assignments_Student/src/assignments/m4/studentdb/DatabaseConnectionTest.java
@@ -0,0 +1,27 @@
+package assignments.m4.studentdb;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/** Provided setup helper: verifies the Module 4 MariaDB environment. */
+public class DatabaseConnectionTest {
+    public static void main(String[] args) {
+        String sql = "SELECT * FROM Students ORDER BY student_id";
+        try (Connection connection = Db.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            System.out.println("SUCCESS: Connected to MariaDB!");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("student_id") + " | "
+                    + resultSet.getString("first_name") + " | "
+                    + resultSet.getString("last_name") + " | "
+                    + resultSet.getString("email") + " | "
+                    + resultSet.getString("major"));
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR: Database connection failed.");
+            e.printStackTrace();
+        }
+    }
+}
