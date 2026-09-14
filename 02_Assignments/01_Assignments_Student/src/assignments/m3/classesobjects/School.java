cfbd30723e3591dda14519524d5599754225f440
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 08:46:19 -0700

    Move M3 student School package under assignments

---
+++ 02_Assignments/01_Assignments_Student/src/assignments/m3/classesobjects/School.java
@@ -0,0 +1,9 @@
+package assignments.m3.classesobjects;
+
+/** M3A: School class starter. */
+public class School {
+    private Student[] students;
+    public School(Student[] students) { this.students = students; }
+    public Student getStudentWithHighestAverage() { /* TODO: Compare student averages. */ return null; }
+    public Student getStudentWithLowestAverage() { /* TODO: Compare student averages. */ return null; }
+}
