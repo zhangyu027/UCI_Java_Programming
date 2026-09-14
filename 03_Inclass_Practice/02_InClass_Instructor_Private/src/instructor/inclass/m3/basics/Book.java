7b49e4badc53e8240aa68db8d9c46594b62808f1
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 10:15:25 -0700

    Align M3 Book with uploaded Canvas starter

--- 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m3/basics/Book.java
+++ 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m3/basics/Book.java
@@ -1,22 +1,53 @@
 package instructor.inclass.m3.basics;
 
-/** Canvas-aligned Module 3 basic class example. */
+/** Complete instructor reference based on the Canvas Book starter. */
 public class Book {
     private String title;
     private String author;
+    private int pages;
+    private double price;
 
-    public Book(String title, String author) {
+    public Book(String title, String author, int pages, double price) {
         this.title = title;
         this.author = author;
+        this.pages = pages;
+        this.price = price;
     }
 
-    public void displayInfo() {
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void read() {
+        System.out.println("Reading " + title + " by " + author + ".");
     }
 
     public static void main(String[] args) {
-        Book book = new Book("Core Java", "Cay Horstmann");
-        book.displayInfo();
+        Book book = new Book("Core Java", "Cay Horstmann", 928, 59.99);
+
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Author: " + book.getAuthor());
+        System.out.println("Pages: " + book.getPages());
+        System.out.println("Price: $" + book.getPrice());
+
+        book.read();
+        book.setPrice(49.99);
+        System.out.println("Updated price: $" + book.getPrice());
     }
 }
