19e94af6260d79e7dcd811d1840cfe3fb8d25962
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 08:58:37 -0700

    Move student M5 recursion under assignments

---
+++ 02_Assignments/01_Assignments_Student/src/assignments/m5/recursion/CombinationGenerator.java
@@ -0,0 +1,16 @@
+package assignments.m5.recursion;
+
+/** M5 Assignment: recursively generate character combinations. */
+public class CombinationGenerator {
+    public static void generateCombinations(String input, String current, int index) {
+        // TODO: Add the base case.
+        // TODO: Add recursive calls that include/exclude the current character.
+        /* HINT: stop at input.length(); then try include and exclude branches. */
+    }
+
+    public static void main(String[] args) {
+        String input = "ABC";
+        // TODO: Call generateCombinations(input, "", 0).
+        System.out.println("M5 Recursion starter ready for input: " + input);
+    }
+}
