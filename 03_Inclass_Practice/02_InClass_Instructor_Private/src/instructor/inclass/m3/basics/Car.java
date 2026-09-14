b82786ebe0cd340da07e44006255a2be0af251a6
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 10:15:12 -0700

    Align M3 Car with uploaded Canvas starter

--- 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m3/basics/Car.java
+++ 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m3/basics/Car.java
@@ -1,26 +1,53 @@
 package instructor.inclass.m3.basics;
 
-/** Canvas-aligned Module 3 basic class example. */
+/** Complete instructor reference based on the Canvas Car starter. */
 public class Car {
     private String make;
     private String model;
+    private int year;
+    private double price;
 
-    public Car(String make, String model) {
+    public Car(String make, String model, int year, double price) {
         this.make = make;
         this.model = model;
+        this.year = year;
+        this.price = price;
     }
 
-    public void start() {
-        System.out.println(make + " " + model + " is starting.");
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public void drive() {
-        System.out.println(make + " " + model + " is driving.");
+        System.out.println(year + " " + make + " " + model + " is driving.");
     }
 
     public static void main(String[] args) {
-        Car car = new Car("Toyota", "Camry");
-        car.start();
+        Car car = new Car("Toyota", "Camry", 2024, 32000.00);
+
+        System.out.println("Make: " + car.getMake());
+        System.out.println("Model: " + car.getModel());
+        System.out.println("Year: " + car.getYear());
+        System.out.println("Price: $" + car.getPrice());
+
         car.drive();
+        car.setPrice(30500.00);
+        System.out.println("Updated price: $" + car.getPrice());
     }
 }
