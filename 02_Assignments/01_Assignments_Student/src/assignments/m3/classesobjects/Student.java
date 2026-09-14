7799d1a4fbf78d29df3fc9aff60fbfb747b9430d
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 10:06:01 -0700

    Align M3 Student homework starter with Canvas rubric

--- 02_Assignments/01_Assignments_Student/src/assignments/m3/classesobjects/Student.java
+++ 02_Assignments/01_Assignments_Student/src/assignments/m3/classesobjects/Student.java
@@ -1,12 +1,37 @@
 package assignments.m3.classesobjects;
 
-/** M3A: Student class starter. */
+/** Module 3 Homework: Student class starter. */
 public class Student {
-    private String name; private int rollNumber; private int[] marks;
-    public Student(String name, int rollNumber, int[] marks) { this.name=name; this.rollNumber=rollNumber; this.marks=marks; }
-    public int getTotalMarks() { /* TODO: Sum all marks. */ return 0; }
-    public double getAverageMarks() { /* TODO: Calculate average. */ return 0.0; }
-    public String getGrade() { /* TODO: Return A-F based on assignment ranges. */ return "TODO"; }
-    public String getName() { return name; }
-    public int getRollNumber() { return rollNumber; }
+    private String name;
+    private int rollNumber;
+    private int[] marks;
+
+    public Student(String name, int rollNumber, int[] marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    public int getTotalMarks() {
+        // TODO: Add all five subject marks and return the total.
+        return 0;
+    }
+
+    public double getAverageMarks() {
+        // TODO: Return the average of the five marks.
+        return 0.0;
+    }
+
+    public String getGrade() {
+        // TODO: Use the Canvas ranges: A >=90, B >=80, C >=70, D >=60, otherwise F.
+        return "TODO";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
 }
