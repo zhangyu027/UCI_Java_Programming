c061a8a517ffeb3e5849b2ee7d3ce6904951e942
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 08:45:54 -0700

    Move M3 student classes package under assignments

---
+++ 02_Assignments/01_Assignments_Student/src/assignments/m3/classesobjects/Main.java
@@ -0,0 +1,11 @@
+package assignments.m3.classesobjects;
+
+/** M3A: test the Student and School classes. */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("M3 Classes & Objects starter ready.");
+        // TODO: Create at least two Student objects with five marks each.
+        // TODO: Put them into a Student[] array and create a School object.
+        // TODO: Print total, average, grade, highest, and lowest results.
+    }
+}
