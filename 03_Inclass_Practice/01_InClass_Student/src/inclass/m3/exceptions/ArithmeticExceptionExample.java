0eea5f0f041bd4e56a3deb1540b50414777ec47b
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 10:07:15 -0700

    Add Canvas-aligned arithmetic exception student starter

---
+++ 03_Inclass_Practice/01_InClass_Student/src/inclass/m3/exceptions/ArithmeticExceptionExample.java
@@ -0,0 +1,17 @@
+package inclass.m3.exceptions;
+
+/** Canvas Example 1 student starter: Handling ArithmeticException. */
+public class ArithmeticExceptionExample {
+    public static void main(String[] args) {
+        try {
+            // TODO 1: Call divide(10, 0) and print the result.
+        } catch (ArithmeticException e) {
+            // TODO 2: Print a meaningful arithmetic-exception message.
+        }
+    }
+
+    public static int divide(int dividend, int divisor) {
+        // TODO 3: Return dividend divided by divisor.
+        return 0;
+    }
+}
