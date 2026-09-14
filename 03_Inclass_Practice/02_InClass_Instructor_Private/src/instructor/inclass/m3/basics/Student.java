cd59439577ea7ba0e1d7cb3d53201c90164d58eb
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 10:15:36 -0700

    Align M3 Student with uploaded Canvas starter

--- 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m3/basics/Student.java
+++ 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m3/basics/Student.java
@@ -1,25 +1,61 @@
 package instructor.inclass.m3.basics;
 
+/** Complete instructor reference based on the Canvas Student starter. */
 public class Student {
     private String name;
-    private int[] marks;
-    public Student(String n, int[]m) {
-        name=n;
-        marks=m.clone();
+    private int rollNumber;
+    private int[] marks = new int[5];
+
+    public Student(String name, int rollNumber, int[] marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks.clone();
     }
+
     public String getName() {
         return name;
     }
-    public int total() {
-        int s=0;
-        for (int x:marks)s+=x;
-        return s;
+
+    public int getRollNumber() {
+        return rollNumber;
     }
-    public double average() {
-        return total()/(double)marks.length;
+
+    public int getTotalMarks() {
+        int total = 0;
+        for (int mark : marks) {
+            total += mark;
+        }
+        return total;
     }
-    public String grade() {
-        double a=average();
-        return a>=90?"A":a>=80?"B":a>=70?"C":a>=60?"D":"F";
+
+    public double getAverageMarks() {
+        return getTotalMarks() / (double) marks.length;
+    }
+
+    public String getGrade() {
+        double average = getAverageMarks();
+
+        if (average >= 90) {
+            return "A";
+        } else if (average >= 80) {
+            return "B";
+        } else if (average >= 70) {
+            return "C";
+        } else if (average >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] marks = {95, 88, 92, 90, 85};
+        Student student = new Student("Alex Chen", 101, marks);
+
+        System.out.println("Name: " + student.getName());
+        System.out.println("Roll Number: " + student.getRollNumber());
+        System.out.println("Total Marks: " + student.getTotalMarks());
+        System.out.println("Average Marks: " + student.getAverageMarks());
+        System.out.println("Grade: " + student.getGrade());
     }
 }
