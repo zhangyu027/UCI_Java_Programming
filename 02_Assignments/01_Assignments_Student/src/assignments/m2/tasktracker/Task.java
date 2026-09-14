9f3a89c0be6f4d6b1976002a30456e5f624a6f10
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 08:45:24 -0700

    Move M2 student Task package under assignments

---
+++ 02_Assignments/01_Assignments_Student/src/assignments/m2/tasktracker/Task.java
@@ -0,0 +1,21 @@
+package assignments.m2.tasktracker;
+
+import java.util.Date;
+
+/** Module 2 Homework Task data class based on the Canvas homework. */
+public class Task {
+    private String title;
+    private String description;
+    private Date dueDate;
+    // TODO: Add a field that records whether this task is complete.
+
+    public Task(String title, String description, Date dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public Date getDueDate() { return dueDate; }
+    // TODO: Add methods needed to read/change completion status.
+}
