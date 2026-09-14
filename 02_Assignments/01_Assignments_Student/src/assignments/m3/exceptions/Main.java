7bdda89ac3972482d766fb6a828f96cc151719da
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 08:46:55 -0700

    Move M3 student exception driver under assignments

---
+++ 02_Assignments/01_Assignments_Student/src/assignments/m3/exceptions/Main.java
@@ -0,0 +1,16 @@
+package assignments.m3.exceptions;
+
+import java.util.Scanner;
+
+/** M3B: Calculator + exception-control driver. */
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        try (Scanner scanner = new Scanner(System.in)) {
+            // TODO: Prompt for two numbers and an operator.
+            // TODO: Call the correct Calculator method.
+            // TODO: Catch ArithmeticException and both custom exceptions.
+            System.out.println("M3 Exception Control starter ready.");
+        }
+    }
+}
