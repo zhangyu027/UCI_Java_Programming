930e1476fafb7cd9f0b0abbb425f1f5ed35945a7
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 21:11:59 -0700

    Align M5 Tower of Hanoi instructor example with Canvas

--- 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m5/TowerOfHanoi.java
+++ 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m5/TowerOfHanoi.java
@@ -1,16 +1,27 @@
 package instructor.inclass.m5;
 
+/** Recursive Java program to solve the Tower of Hanoi problem. */
 public class TowerOfHanoi {
+
     public static void towerOfHanoi(int n, char source, char auxiliary, char destination) {
-        if (n==1) {
-            System.out.println("Move disk 1 from "+source+" to "+destination);
+        // Base case: move one disk directly to the destination
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
             return;
         }
-        towerOfHanoi(n-1, source, destination, auxiliary);
-        System.out.println("Move disk "+n+" from "+source+" to "+destination);
-        towerOfHanoi(n-1, auxiliary, source, destination);
+
+        // Move n-1 disks from source to auxiliary
+        towerOfHanoi(n - 1, source, destination, auxiliary);
+
+        // Move the nth disk from source to destination
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+
+        // Move n-1 disks from auxiliary to destination
+        towerOfHanoi(n - 1, auxiliary, source, destination);
     }
-    public static void main(String[]a) {
-        towerOfHanoi(3, 'A', 'B', 'C');
+
+    public static void main(String[] args) {
+        int numberOfDisks = 3;
+        towerOfHanoi(numberOfDisks, 'A', 'B', 'C');
     }
 }
