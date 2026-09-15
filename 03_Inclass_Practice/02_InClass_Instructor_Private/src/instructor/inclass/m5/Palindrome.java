6b98978e9a9718d8845f7f194a25cb3a98c0d314
Yu Zhang <zhangyu027@gmail.com>
2026-09-14 21:11:37 -0700

    Align M5 Palindrome instructor example with Canvas

--- 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m5/Palindrome.java
+++ 03_Inclass_Practice/02_InClass_Instructor_Private/src/instructor/inclass/m5/Palindrome.java
@@ -1,13 +1,29 @@
 package instructor.inclass.m5;
 
+/** Recursive Java program to check if a given string is a palindrome. */
 public class Palindrome {
-    public static boolean isPalindrome(String s, int start, int end) {
-        if (start>=end)return true;
-        if (s.charAt(start)!=s.charAt(end))return false;
-        return isPalindrome(s, start+1, end-1);
+
+    public static boolean isPalindrome(String str, int start, int end) {
+        // Base case: all characters have matched
+        if (start >= end) {
+            return true;
+        }
+
+        // Recursive case: outside characters must match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(str, start + 1, end - 1);
     }
-    public static void main(String[]a) {
-        String s="racecar";
-        System.out.println(s+" palindrome = "+isPalindrome(s, 0, s.length()-1));
+
+    public static void main(String[] args) {
+        String testStr = "racecar";
+
+        if (isPalindrome(testStr, 0, testStr.length() - 1)) {
+            System.out.println(testStr + " is a palindrome.");
+        } else {
+            System.out.println(testStr + " is not a palindrome.");
+        }
     }
 }
